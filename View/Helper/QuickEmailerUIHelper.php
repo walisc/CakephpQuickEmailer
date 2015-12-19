b7b7a3dde1d85da3adeb7824b3759b6903127cd4
chido <chidow@j5int.com>
2015-12-19 20:36:57 +0200

    Adding cake emailer files

---
+++ View/Helper/QuickEmailerUIHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+App::uses('AppHelper', 'View/Helper');
+
+class QuickEmailerUIHelper extends AppHelper
+{
+
+    public function GetEmailPage()
+    {
+        return '<h1> Emailer </h1>';
+    }
+
+    public function GetEmailModularPage()
+    {
+
+    }
+}
