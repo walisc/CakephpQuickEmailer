2141eebbdbb9cad1574a555c2f823e953e71a413
chido <chidow@j5int.com>
2015-12-24 22:05:55 +0200

    Adding the email component include API that can be used

---
+++ Controller/Component/DALComponent.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chidow
+ * Date: 2015/12/24
+ * Time: 9:38 PM
+ */
+
+App::uses('Component', 'Controller');
+
+class DALComponent extends Component
+{
+    public function initialize(Controller $controller)
+    {
+        
+    }
+}
---
+++ Controller/Component/QuickEmailerAPIComponent.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chidow
+ * Date: 2015/12/19
+ * Time: 8:45 PM
+ */
+
+App::uses('Component', 'Controller');
+App::uses('View', 'View');
+
+class QuickEmailerAPIComponent extends Component
+{
+    public $components = array('QuickEmailer.DALComponent');
+
+    public function SendEmail()
+    {
+
+    }
+
+    public function SaveAsDraft()
+    {
+
+    }
+
+    public function SaveAsTemplate()
+    {
+
+    }
+
+
+}
