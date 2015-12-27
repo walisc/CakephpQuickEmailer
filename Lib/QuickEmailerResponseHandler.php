ce6a8a298df99284bac970dc9af90876570515ea
chido <chidow@j5int.com>
2015-12-27 18:30:54 +0200

    Updating the Response Handler. Adding the default feedback method.

--- Lib/QuickEmailerResponseHandler.php
+++ Lib/QuickEmailerResponseHandler.php
@@ -6,17 +6,33 @@
  * Time: 1:58 PM
  */
 
-class QuickEmailerResponseHandler{
+class QuickEmailerResponseHandler
+{
 
     const ERROR = "Error";
     const GENERAL = "General";
+
+    private static function GetGenericMessage()
+    {
+        return array(
+            'status_message' => 'Failed',
+            'message_summary' => "Ops! Something went wrong:(",
+            'message_content' => "Please try refresh the page. If that doesn't work, please try again later or contact the site Administrator",
+            'response_function' => 'show_modal'
+        );
+    }
 
     public static function RESPOND($type, $response_array)
     {
         if ($type == QuickEmailerResponseHandler::ERROR) {
 
-            //if debug 0
-            return new CakeResponse(array('body' => json_encode($response_array), 'status' => 200));
+            if (Configure::read("debug") != 0) {
+                return new CakeResponse(array('body' => json_encode($response_array), 'status' => 200));
+            }
+            else
+            {
+                return new CakeResponse(array('body' => json_encode(QuickEmailerResponseHandler::GetGenericMessage()), 'status' => 200));
+            }
         }
         elseif ($type == QuickEmailerResponseHandler::GENERAL)
         {
